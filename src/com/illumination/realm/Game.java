b5343cb136aa549b733518d9e66ddf5291226cc8
Illumi-Nation Studios <illnation.3@gmail.com>
2016-10-04 20:23:48 -0500

    Commit #1

---
+++ src/com/illumination/realm/Game.java
@@ -0,0 +1,129 @@
+package com.illumination.realm;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import com.illumination.realm.graphics.Screen;
+
+@SuppressWarnings({ "serial", "unused"})
+public class Game extends Canvas implements Runnable {
+	public static int width = 400;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	public static int sWidth = width * scale;
+	public static int sHeight = height * scale;
+	
+	public static String title = "RotMG Pre-Alpha |";
+
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+	public Game() {
+		Dimension size = new Dimension(sWidth, sHeight);
+		setPreferredSize(size);
+
+		screen = new Screen(width, height);
+
+		frame = new JFrame();
+	}
+
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Realm of the Mad God");
+		System.out.println("Starting");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		System.out.println("Running");
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(title + frames + "|");
+				updates = 0;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	public void update() {
+	}
+
+	public void render() {
+		System.out.println("Rendering");
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		screen.clear();
+
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
+		Graphics g = bs.getDrawGraphics();
+
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+
+		game.frame.setResizable(false);
+		game.frame.setTitle(Game.title + "0|");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		game.start();
+	}
+}
