f5f2997ad45a3e5f08fd917dfe45a641edebbd64
Illumi-Nation Studios <illnation.3@gmail.com>
2016-12-25 19:33:48 -0600

    Commit #12

--- src/com/illumination/realm/entities/mobs/Mob.java
+++ src/com/illumination/realm/entities/mobs/Mob.java
@@ -9,7 +9,35 @@ public abstract class Mob extends Entity {
 	protected int direction = 0;
 	protected boolean moving = false;
 
-	public void move() {
+	public char facing;
+	
+	public char north = 'N';
+	public char south = 'S';
+	public char east = 'E';
+	public char west = 'W';
+	
+	public void move(int xAxis, int yAxis) {
+		if (xAxis > 0) {
+			direction = 1;
+			facing = east;
+		}
+		if (xAxis < 0) {
+			direction = 2;
+			facing = west;
+		}
+		if (yAxis > 0) {
+			direction = 3;
+			facing = south;
+		}
+		if (yAxis < 0) {
+			direction = 0;
+			facing = north;
+		}
+
+		if (!collision()) {
+			x += xAxis;
+			y += yAxis;
+		}
 	}
 
 	public void update() {
